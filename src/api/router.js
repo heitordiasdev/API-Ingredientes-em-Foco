9dae758f8952a780b5fde2caa91ada81caa32c6a
Rubens Lima <rubenslima000@gmail.com>
2022-06-14 14:55:57 -0300

    update new routers

--- src/api/router.js
+++ src/api/router.js
@@ -1,10 +1,16 @@
 const express = require('express');
-
 const router = express.Router()
 
+const home = require('../controller/home');
+const food = require('../controller/food');
+const users = require('../controller/users');
 
-router.get('/',(req, res) => {
-    return res.send("App funcionando");
-});
+router.get('/', home.paginaHome);
+router.get('/food', food.paginaFood);
+router.get('/users', users.paginaUsers);
+
+// router.get('/',(req, res) => {
+//     return res.send("App funcionando");
+// });
 
 module.exports = router
