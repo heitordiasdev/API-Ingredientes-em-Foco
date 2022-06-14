dd3d53348ba920e6f5e518805d1c652a353bb147
Rubens Lima <rubenslima000@gmail.com>
2022-06-14 14:56:40 -0300

    update new functions app

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const { sequelize } = require('./models')
 //APP
 const app = express();
 app.use(express.json());
-app.use('/', router);
+app.use(router);
 
 //DATABASES
 sequelize.sync().then(() => {
@@ -14,4 +14,8 @@ sequelize.sync().then(() => {
 
 
 // PORT
-app.listen(3000, () => console.log("App funcionando"));
+const link = 'http://localhost:3000';
+app.listen(3000, () => {
+    console.log("App funcionando");
+    console.log(`Link: ${link}`);
+});
