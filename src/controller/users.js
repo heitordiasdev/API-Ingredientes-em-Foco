f9b40d974fffd37bc716fd42274228de577e25a1
Igor Silva <igorpas93@gmail.com>
2022-06-16 18:09:30 -0300

    Created user service

--- src/controller/users.js
+++ src/controller/users.js
@@ -1,43 +1,39 @@
 const express = require('express')
 const router = express.Router()
 const { user } = require('../models')
+const UserService = require('../services/user')
+
+const userService = new UserService(user);
 
 router.get('/', async (req, res) => {
-    const listUser = await user.findAll()
+    const listUser = await userService.getAll()
     res.json(listUser)
 })
 
 router.get('/:id', async (req, res) => {
     const id = req.params.id;
-    const selectUser = await user.findByPk(id)
+    const selectUser = await userService.getId(id)
     res.json(selectUser)
 })
 
 router.post('/', async (req, res) => {
     const { name, cpfCnpj, email, password, dateNasc, typeUser } = req.body
-    const newUser = await user.create({ name, cpfCnpj, email, password, dateNasc, typeUser })
+    const newUser = await userService.add({ name, cpfCnpj, email, password, dateNasc, typeUser })
     res.json(newUser)
 })
 
 router.delete('/:id', async (req, res) => {
     const id = req.params.id;
-    await user.destroy({
-        where: { id: id }
-    })
+    await userService.deleteUser(id)
     res.send('Usuário deletado com sucesso!')
 })
 
 router.put('/:id', async (req, res) => {
     const { name, cpfCnpj, email, password, dateNasc, typeUser } = req.body
     const id = req.params.id;
-    const userupdated = await user.update({
-        name, cpfCnpj, email, password, dateNasc, typeUser
-    },
-        {
-            where: { id: id }
-        })
+    const userupdated = await userService.editUser(id, { name, cpfCnpj, email, password, dateNasc, typeUser })
     res.send('Usuário atualizado com sucesso!')
 })
 
 
-module.exports = router 
+module.exports = router
