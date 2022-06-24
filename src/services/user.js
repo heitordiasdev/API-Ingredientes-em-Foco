596e7db71cc06765aeb58582d03a7700e2172b55
Rubens Lima <rubenslima000@gmail.com>
2022-06-24 15:18:20 -0300

    organizando algumas declarações no user

--- src/services/user.js
+++ src/services/user.js
@@ -1,48 +1,58 @@
 class UserService {
   constructor(UserModel) {
-    this.user = UserModel
+    this.user = UserModel;
   }
 
   async getAll() {
     try {
-      const listUser = await this.user.findAll()
-      return listUser
+      const listUser = await this.user.findAll();
+      return listUser;
     } catch (erro) {
-      console.error(erro.message)
-      throw erro
+      console.error(erro.message);
+      throw erro;
     }
   }
 
   async getId(id) {
     try {
-      const selectUser = await this.user.findByPk(id)
-      return selectUser
+      const selectUser = await this.user.findByPk(id);
+      return selectUser;
     } catch (erro) {
-      console.error(erro.message)
-      throw erro
+      console.error(erro.message);
+      throw erro;
     }
   }
 
   async add(userData) {
     try {
-      const newUser = await this.user.create(
-          { 
-            "name"    : userData.name,
-            "cpfCnpj" : userData.cpfCnpj,
-            "email"   : userData.email,
-            "password": userData.password,
-            "dateNasc": userData.dateNasc,
-            "typeUser": userData.typeUser
-          }
-        )
-      return {code: 201, message: "Usuário criado com sucesso!", data:newUser}
+      const newUser = await this.user.create({
+        name: userData.name,
+        cpfCnpj: userData.cpfCnpj,
+        email: userData.email,
+        password: userData.password,
+        dateNasc: userData.dateNasc,
+        typeUser: userData.typeUser,
+      });
+      return {
+        code: 201,
+        message: "Usuário criado com sucesso!",
+        data: newUser,
+      };
     } catch (error) {
-      if (error.constructor.name == 'ValidationError' ) {
-        console.error(error)
-        return {code: 422, message: "Erro ao tentar cadastrar usuário!", data:{message_error:error.message}}
-      }else{
-        console.error(error)
-        return {code: 500, message: "Erro ao tentar cadastrar usuário!", data:{message_error:error.message}}
+      if (error.constructor.name == "ValidationError") {
+        console.error(error);
+        return {
+          code: 422,
+          message: "Erro ao tentar cadastrar usuário!",
+          data: { message_error: error.message },
+        };
+      } else {
+        console.error(error);
+        return {
+          code: 500,
+          message: "Erro ao tentar cadastrar usuário!",
+          data: { message_error: error.message },
+        };
       }
     }
   }
@@ -50,11 +60,11 @@ class UserService {
   async deleteUser(id) {
     try {
       await this.user.destroy({
-        where: { id: id }
-      })
+        where: { id: id },
+      });
     } catch (erro) {
-      console.error(erro.message)
-      throw erro
+      console.error(erro.message);
+      throw erro;
     }
   }
 
@@ -62,22 +72,21 @@ class UserService {
     try {
       const userUpdate = await this.user.update(
         {
-          "name": userData.name,
-          "cpfCnpj": userData.cpfCnpj,
-          "email": userData.email,
-          "password": userData.password,
-          "dateNasc": userData.dateNasc,
-          "typeUser": userData.typeUser
+          name: userData.name,
+          cpfCnpj: userData.cpfCnpj,
+          email: userData.email,
+          password: userData.password,
+          dateNasc: userData.dateNasc,
+          typeUser: userData.typeUser,
         },
         { where: { id: id } }
-      )
-      return userUpdate
+      );
+      return userUpdate;
     } catch (erro) {
-      console.error(erro.message)
-      throw erro
+      console.error(erro.message);
+      throw erro;
     }
   }
-
 }
 
 module.exports = UserService;
